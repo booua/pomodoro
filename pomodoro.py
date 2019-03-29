e312d9ccf31ef502ead3c788b8f22b6b6db9a7d4
Jacek Tadeusz Paciorek <jacek.tadeusz.paciorek@cern.ch>
2019-03-29 14:52:32 +0100

    add timer, replace notify methods for show_toast (windows)

--- pomodoro.py
+++ pomodoro.py
@@ -1,39 +1,39 @@
-import os
 import sys
 import time
+from win10toast import ToastNotifier
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
 
 class PomodoroTimer():
 
-    def __init__(self, workTime, brakeTime):
+    def __init__(self, workTime, brakeTime, intervals):
         self.workTime = workTime
         self.brakeTime = brakeTime
-
-    @staticmethod
-    def notify(title, text):
-        os.system(
-            """osascript -e 'display notification "{}" with title "{}"'""".format(text, title))
-
-    @staticmethod
-    def sayNotification(message):
-        os.system('say "{}"'.format(message))
+        self.intervals = intervals
+    
 
     def countdownWorktime(self):
         workTime = self.workTime * 60
         brakeTime = self.brakeTime * 60
-        for index in range(4):
-            print("It's worktime")
-            self.notify("Worktime", "Shut the fuck up and work")
-            self.sayNotification("Its time to work mother fucka")
-            time.sleep(workTime)
-            print("brake")
-            self.notify("Brake", "Brake time baby")
-            self.sayNotification("Brake time baby")
-            time.sleep(brakeTime)
-
-if len(sys.argv[1:]) != 2:
-    pomodoro = PomodoroTimer(25, 5)
+        intervals = self.intervals
+        toaster = ToastNotifier()
+        for index in range(intervals):
+            print("Worktime, yo")
+            toaster.show_toast("Worktime","Shut the hell up and WORK!")
+            countdown(workTime)
+            print("Brake time baby")
+            toaster.show_toast("Brake","Brake time baby")
+            countdown(brakeTime)
+            print("Interval: "+str(index+1))
+if  len(sys.argv) < 3:
+    pomodoro = PomodoroTimer(25, 5, 4)
     pomodoro.countdownWorktime()
-
 else:
-    pomodoro = PomodoroTimer(float(sys.argv[1:][0]), float(sys.argv[1:][1]))
+    pomodoro = PomodoroTimer(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
     pomodoro.countdownWorktime()
